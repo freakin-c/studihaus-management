594352f2d8f2126ecf7c61f94ab18f32651d981c
Leon Alef <leon@alef.one>
2018-10-21 12:34:25 +0200

    Setup translation & add translation to german
    Import djangos gettext method as _ in app_residents models.py
    and vies.py following django conventions.
    
    Add translation file for language code 'de' with updated meta data
    strings. Update settings.py (and .default) accordingly.
    
    gitignore *.mo files (binary compiled messages/translation files)

--- app_residents/models.py
+++ app_residents/models.py
@@ -1,22 +1,24 @@
 from django.db import models
+from django.utils.translation import gettext as _
+
 from datetime import date, timedelta
 
 class Resident(models.Model):
 
-    forename = models.CharField('Forename', max_length=255)
-    surname = models.CharField('Surname', max_length=255)
-    email = models.EmailField('E-Mail')
-    mobile_number = models.CharField('Mobile number', max_length=20, blank=True)
-    date_move_in = models.DateField('Date of move-in', help_text='Date of move-in like on rental agreement', default=date.today)
-    date_move_out = models.DateField('Date of move-out', help_text='Date of move-out', null=True, blank=True)
+    forename = models.CharField(_('Forename'), max_length=255)
+    surname = models.CharField(_('Surname'), max_length=255)
+    email = models.EmailField(_('E-Mail'))
+    mobile_number = models.CharField(_('Mobile number'), max_length=20, blank=True)
+    date_move_in = models.DateField(_('Date of move-in'), help_text=_('Date of move-in like stated on rental agreement'), default=date.today)
+    date_move_out = models.DateField(_('Date of move-out'), help_text=_('Date of move-out'), null=True, blank=True)
 
     def __str__(self):
         return self.forename
 
 class Room(models.Model):
-    room_code = models.CharField('Room code', help_text='Room code like "HH32"', max_length=4, unique=True)
-    rent = models.DecimalField('Rent', max_digits=5, decimal_places=2)
-    resident = models.OneToOneField(Resident, on_delete=models.PROTECT, related_name='room', null=True, blank=True)
+    room_code = models.CharField(_('Room code'), help_text=_('Room code like "HH32"'), max_length=4, unique=True)
+    rent = models.DecimalField(_('Rent'), max_digits=5, decimal_places=2)
+    resident = models.OneToOneField(Resident, verbose_name=_('Resident'), on_delete=models.PROTECT, related_name='room', null=True, blank=True)
 
     def __str__(self):
         return self.room_code + (f' ({self.resident})' if self.resident else '')
--- app_residents/views.py
+++ app_residents/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils.translation import gettext as _
+
 from .models import Resident
 
 from datetime import timedelta, date
 
 def index(request):
-    return HttpResponse('Hello, Goebenstift')
+    return HttpResponse(_('Hello, Goebenstift!'))
 
 def residents(request):
     current_residents = Resident.objects.filter(date_move_out=None)
