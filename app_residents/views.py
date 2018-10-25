5adff027d89f2d420b280a5f2c520a7e9df7a38c
Leon Alef <leon@alef.one>
2018-10-25 12:45:54 +0200

    Render template residents.html in residents() view
    Separate html from views
    
    * by removing placeholer template string from residents()
    * and adding residents.html and base.html
    
    residents.html extends base.html.

--- app_residents/views.py
+++ app_residents/views.py
@@ -10,5 +10,5 @@ def index(request):
     return HttpResponse(_('Hello, Goebenstift!'))
 
 def residents(request):
-    current_residents = Resident.objects.filter(date_move_out=None)
-    return HttpResponse('<br>'.join([f'{resident}: {resident.room.room_code} - {resident.email}' for key, resident in enumerate(current_residents)]))
+    residents = Resident.objects.filter(date_move_out=None)
+    return render(request, 'residents.html', {'residents': residents})
