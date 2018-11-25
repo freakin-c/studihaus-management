f63f180796ba9030e40114f17a063b5719e8b276
Leon Alef <leon@alef.one>
2018-11-25 16:34:15 +0100

    Add model Tenancy to admin

--- app_residents/admin.py
+++ app_residents/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Resident, Room
+from .models import Resident, Room, Tenancy
 
 admin.site.register(Room)
 admin.site.register(Resident)
+admin.site.register(Tenancy)
